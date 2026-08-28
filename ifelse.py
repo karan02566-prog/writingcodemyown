51b7e5e13a0e3b525e88eeca535b9fa36ab2ced5
karan <karan.02566@gmail.com>
2026-08-28 23:36:53 +0530

    Add ifelse practice

---
+++ ifelse.py
@@ -0,0 +1,12 @@
+a=int(input("enter your age: "))
+if (a>18):
+    print("you're eligible for club entry")
+    print("also you can buy or sell drugs")
+    print("just don't get caught by undercover police")
+else:
+    print("get out from here")
+    print("you should be at your home")
+    print("i'll call your parents and tell them about your age")
+    print("get the fuck out of here you little brat")
+print(a>=18)
+print(a<=20)
