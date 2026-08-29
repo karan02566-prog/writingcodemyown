9548f995928a2b5f34f98f5d8e9a61f772eae669
karan <karan.02566@gmail.com>
2026-08-29 11:09:24 +0530

    add elif practice

---
+++ elif.py
@@ -0,0 +1,18 @@
+muskan_neet_score=int(input("enter your neet marks: "))
+if (muskan_neet_score>700):
+    print("aiims delhi pakka ab to")
+    
+elif (muskan_neet_score>=680 and muskan_neet_score<=699):
+        print("vmmc abhi bhi mil skta hai")
+        print("hopium")
+elif (muskan_neet_score>=670 and muskan_neet_score<=679):
+        print("aiims to mil jayega")
+elif (muskan_neet_score>=650 and muskan_neet_score<=669):
+        print("hopium")
+else:
+    print("koi na next year fir se try krenge")
+
+
+
+
+    
