676626af4be9995f767bcf898dd07a8119389063
karan <karan.02566@gmail.com>
2026-08-30 09:00:14 +0530

    match case 2 practice

---
+++ mcase2.py
@@ -0,0 +1,40 @@
+x=input("enter the color: ")
+
+match x :
+    case "red" :
+        print("stop!!!!")
+    case "yellow" :
+        print("stop watch if safe then go")
+    case "green" :
+        print("youcan goooo!!!!!!")
+
+    case _ :
+        print("invalid input")
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
