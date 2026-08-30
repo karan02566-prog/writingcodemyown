7fba07fb149a18285453c2ccf72ed82141b0bfb8
karan <karan.02566@gmail.com>
2026-08-30 11:25:46 +0530

    loops practice

---
+++ forloops.py
@@ -0,0 +1,11 @@
+# colours=("red", "green", "yellow")
+# for i in colours:
+#     print(i)
+#     for color in i:
+#         print(color)
+
+# for k in range(20):
+#     print(k)
+
+for s in range(0, 20, 2):
+    print(s)
