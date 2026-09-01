12b204a8e78432dc9785f5d21445894a811cea4a
karan <karan.02566@gmail.com>
2026-09-01 23:43:48 +0530

    practice functions and while loop

---
+++ function.py
@@ -0,0 +1,25 @@
+def calculateGmean(a,b):
+    mean = (a*b)/(a+b)
+    print(mean)
+
+def isgreater(a, b):
+    if(a>b):
+        print("a is greater than b.")
+    else:
+        ("b is greater than a")
+
+a=8
+b=5
+calculateGmean(a, b)
+
+c=9
+e=24
+calculateGmean(c, e)
+
+# def add(a, b):     # a, b are PARAMETERS (placeholders)
+#     return a + b
+
+# add(3, 5)           # 3, 5 are ARGUMENTS (actual values passed in)
+
+
+    
---
+++ whileloops.py
@@ -0,0 +1,18 @@
+# k = 2
+# while (k<50):
+#     print(k)
+#     k = k + 2
+
+# print("done with numbers")
+
+# x=int(input("enter your number: "))
+# while(x<=100):
+#     x=int(input("enter the number: "))
+#     print(x)
+# else:
+#     print("it's done bro")
+
+k=20
+while(k<90):
+    print(k)
+    
