397dc29720275fe10ab140c13641ca312e66c92c
karan <karan.02566@gmail.com>
2026-09-02 23:58:08 +0530

    function more lines

--- function.py
+++ function.py
@@ -1,25 +1,36 @@
+
+
+
+
 def calculateGmean(a,b):
     mean = (a*b)/(a+b)
     print(mean)
 
 def isgreater(a, b):
     if(a>b):
-        print("a is greater than b.")
+        print("first number is greater")
     else:
-        ("b is greater than a")
+        print("second number is greater")
 
 a=8
 b=5
+
+isgreater(a, b)
+
+
 calculateGmean(a, b)
 
 c=9
 e=24
 calculateGmean(c, e)
 
+isgreater(c, e)
+    
 # def add(a, b):     # a, b are PARAMETERS (placeholders)
 #     return a + b
 
 # add(3, 5)           # 3, 5 are ARGUMENTS (actual values passed in)
 
 
-    
+def islesser(a,b):
+    pass
