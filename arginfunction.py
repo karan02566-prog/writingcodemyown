5d4c3cafee5d41d29d14464b513137d0fb32f777
karan <karan.02566@gmail.com>
2026-09-03 23:22:17 +0530

    arguements in function

---
+++ arginfunction.py
@@ -0,0 +1,11 @@
+# def isgreater(a, b):
+#     if a>b :
+#         print("number 1 is greater than b")
+#     else:
+#         print("number 2 is equals to or greater than number 1")
+
+# isgreater(8, 9)
+
+# def greet(name):
+#     print("hello", name, "how are you?")
+# greet("karan")
