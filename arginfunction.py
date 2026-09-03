533c90563aba09bb49916b594c8c0556d348a911
karan <karan.02566@gmail.com>
2026-09-03 23:31:31 +0530

    multiple arguements

--- arginfunction.py
+++ arginfunction.py
@@ -10,13 +10,26 @@
 #     print("hello", name, "how are you?")
 # greet("karan")
 
-def double(x):
-    print(x*x)
-double(25)
+# def double(x):
+#     print(x*x)
+# double(25)
 
 #positional arguement
 # the position you write parameters or placeholder in the sequence python take values as same as them.
 
-def subtract(a, b):
-    print(a-b)
-subtract(25, 21)
+# def subtract(a, b):
+#     print(a-b)
+# subtract(25, 21)
+
+# multiple arguement
+
+def average(a, b, c, d):
+    result = (a+b+c+d)/4
+    print(result)
+average(12, 24, 56, 89)
+
+
+def sum(a, b, c):
+    rizz = (a+b+c)
+    print(rizz)
+sum(777, 888, 999)
