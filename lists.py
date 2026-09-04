d6a899ef6773a89ba4c475d1848bdebdb3b9b391
karan <karan.02566@gmail.com>
2026-09-04 13:38:06 +0530

    lists

--- lists.py
+++ lists.py
@@ -7,3 +7,12 @@ pending_tasks = []
 print(scores)
 print(subjects)
 print(pending_tasks)
+
+print(scores[0])
+print(scores[3])
+print(subjects[2])
+
+if any(80 <= score <= 99 for score in scores):
+    print("you're good at studies")
+else:
+    print("you need to work hard")
