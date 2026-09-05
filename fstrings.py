dcb2b318b0351ed5fc07d9f949913247841c03dd
karan <karan.02566@gmail.com>
2026-09-05 20:10:23 +0530

    f string new and old method

---
+++ fstrings.py
@@ -0,0 +1,8 @@
+# letter = "my name is {} and i'm {} years old. I want to pursue master's in {} from {} so that i can get PR"
+age = 19
+name = "karan"
+masters = "data science"
+country = "usa"
+
+# print(letter.format(name, age, masters, country)) this was old method
+print(f"my name is {name} and i'm {age} years old and i want to do maste's in{masters} from {country} so that i can get PR")
