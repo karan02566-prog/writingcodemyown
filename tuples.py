8738cc4787b530718855b32ba8e9fefa0578c900
karan <karan.02566@gmail.com>
2026-09-05 18:40:21 +0530

    tuples methods

--- tuples.py
+++ tuples.py
@@ -13,8 +13,24 @@
 # countries = tuple(temp)
 # print(countries)
 
-countries = ("spain", "usa", "china", "india", "new zeleand")
-temp = list(countries)
-temp.pop(4)
-countries = tuple(temp)
-print(countries)
+# countries = ("spain", "usa", "china", "india", "new zeleand")
+# temp = list(countries)
+# temp.pop(4)
+# countries = tuple(temp)
+# print(countries)
+
+# countries2 = ("laoPDR", "finland", "germany", "france")
+# world = countries + countries2
+# print(world)
+
+numbers = (2, 5, 6, 7, 8, 14, 5, 4, 5, 9, 5)
+
+# res = numbers.count(5)
+
+# print(res)
+
+# palm = numbers.index(8)
+# print(palm)
+
+palm = numbers.index(9, 3, 10)
+print(palm)
