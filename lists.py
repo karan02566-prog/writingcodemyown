ab4c607954042cc81c694bdec311621dd272853f
karan <karan.02566@gmail.com>
2026-09-05 18:14:38 +0530

    lists

--- lists.py
+++ lists.py
@@ -16,3 +16,32 @@ if any(80 <= score <= 99 for score in scores):
     print("you're good at studies")
 else:
     print("you need to work hard")
+
+if 50 in scores:
+    print("yes it is there")
+else:
+    print("it's not there")
+
+print(scores[-3])
+
+print(subjects[len(subjects)-1])
+if "english" in subjects:
+    print("yes you can choose english")
+else:
+    print("we don't have english as an option")
+
+print(scores[1:5:2])
+
+# scores.append(55)
+# print(scores)
+
+# scores.sort()
+# print(scores)
+
+scores.insert(5,45)
+print(scores)
+
+x = [56, 49, 23, 72]
+
+scores.extend(x)
+print(scores)
