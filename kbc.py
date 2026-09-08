641dde5905d70aef678b3056ed35b1de6e60e010
karan <karan.02566@gmail.com>
2026-09-08 21:26:56 +0530

    trying to make a kbc game

---
+++ kbc.py
@@ -0,0 +1,20 @@
+questions = [
+    {
+    "question " : "who is prime minister of india ?",
+    "options " : ["narendra modi", "rahul gandhi", "elon musk", "sam alltman"],
+    "answer " : "narendra modi"
+    },
+    {
+        "question " : "who is taking entry level jobs ?",
+        "options " : ["samosewala", "corona virus", "artifical intelligence", "no one"],
+        "answer " : "artifical intelligence"
+    }
+]
+
+for thing in questions:
+    print(thing)
+
+
+
+
+
